a653fa8a2808715beff477487bc4f5e40f4e2f34
James Strassburg <jstrassburg@gmail.com>
2014-09-08 15:50:49 -0500

    Extracting parameters into their own tested class.

---
+++ src/com/lucidworks/analysis/AutoPhrasingParameters.java
@@ -0,0 +1,104 @@
+package com.lucidworks.analysis;
+
+import org.apache.solr.common.params.SolrParams;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Abstracts the parameters (and the parsing of those parameters) used by the auto phrasing parser
+ */
+public class AutoPhrasingParameters {
+    private final String DefaultDownstreamParser = "lucene";
+    private final char DefaultReplaceWhitespaceWith = 'x';
+    private final boolean DefaultIgnoreCase = true;
+    private final boolean DefaultEmitSingleTokens = false;
+
+    public AutoPhrasingParameters(SolrParams solrParams) {
+        if (solrParams == null) {
+            downstreamParser = DefaultDownstreamParser;
+            replaceWhitespaceWith = DefaultReplaceWhitespaceWith;
+            ignoreCase = DefaultIgnoreCase;
+            phraseSetFiles = null;
+            emitSingleTokens = DefaultEmitSingleTokens;
+        } else {
+            setDownstreamParser(solrParams.get("defType", DefaultDownstreamParser));
+            setReplaceWhitespaceWith(solrParams.get("replaceWhitespaceWith",
+                    Character.toString(DefaultReplaceWhitespaceWith)));
+            setIgnoreCase(solrParams.getBool("ignoreCase", DefaultIgnoreCase));
+            setPhraseSetFiles(solrParams.get("phrases"));
+            setEmitSingleTokens(solrParams.getBool("includeTokens", DefaultEmitSingleTokens));
+        }
+    }
+
+    /**
+     * Getter for a comma separated string of file names containing auto phrases
+     * @return a list of the parsed individual file names
+     */
+    public List<String> getIndividualPhraseSetFiles() {
+        if (phraseSetFiles == null) {
+            return Collections.emptyList();
+        }
+
+        List<String> result = new ArrayList<String>();
+        for (String file : phraseSetFiles.split("(?<!\\\\),")) {
+            result.add(file.replaceAll("\\\\(?=,)", ""));
+        }
+
+        return result;
+    }
+
+    public String getPhraseSetFiles() {
+        return this.phraseSetFiles;
+    }
+
+    /**
+     * Setter for a comma separated string of files containing autophrase entries
+     * @param phraseSetFiles The comma separated string of files containing autophrase entries
+     */
+    public void setPhraseSetFiles(String phraseSetFiles) {
+        this.phraseSetFiles = phraseSetFiles;
+    }
+
+    public char getReplaceWhitespaceWith() {
+        return replaceWhitespaceWith;
+    }
+
+    public void setReplaceWhitespaceWith(String replaceWhitespaceWith) {
+        if (replaceWhitespaceWith != null && replaceWhitespaceWith.length() > 0)
+            this.replaceWhitespaceWith = replaceWhitespaceWith.charAt(0);
+        else
+            this.replaceWhitespaceWith = DefaultReplaceWhitespaceWith;
+    }
+
+    public boolean getIgnoreCase() {
+        return ignoreCase;
+    }
+
+    public void setIgnoreCase(boolean ignoreCase) {
+        this.ignoreCase = ignoreCase;
+    }
+
+    public String getDownstreamParser() {
+        return downstreamParser;
+    }
+
+    public void setDownstreamParser(String downstreamParser) {
+        this.downstreamParser = downstreamParser;
+    }
+
+    public boolean getEmitSingleTokens() {
+        return emitSingleTokens;
+    }
+
+    public void setEmitSingleTokens(boolean emitSingleTokens) {
+        this.emitSingleTokens = emitSingleTokens;
+    }
+
+    private String downstreamParser;
+    private char replaceWhitespaceWith;
+    private boolean ignoreCase;
+    private String phraseSetFiles;
+    private boolean emitSingleTokens;
+}
---
+++ test/com/lucidworks/analysis/TestAutoPhrasingParameters.java
@@ -0,0 +1,125 @@
+package com.lucidworks.analysis;
+
+import junit.framework.TestCase;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+
+public class TestAutoPhrasingParameters extends TestCase {
+    private final String DefaultDownstreamParser = "lucene";
+    private final char DefaultReplaceWhitespaceWith = 'x';
+    private final boolean DefaultIgnoreCase = true;
+    private final boolean DefaultEmitSingleTokens = false;
+    private final String DefaultPhraseSetFiles = null;
+
+    private final String DownstreamParser = "edismax";
+    private final char ReplaceWhitespaceWith = 'Z';
+    private final boolean IgnoreCase = false;
+    private final String PhrasesOneFile = "phrases.txt";
+    private final String PhrasesMultipleFiles = "phrases.txt,more_phrases.txt";
+    private final boolean EmitSingleTokens = true;
+
+    public void testConstructorNullSolrParams(){
+        AutoPhrasingParameters autoPhrasingParameters = new AutoPhrasingParameters(null);
+        assertNotNull(autoPhrasingParameters);
+        assertEquals(DefaultDownstreamParser, autoPhrasingParameters.getDownstreamParser());
+        assertEquals(DefaultReplaceWhitespaceWith, autoPhrasingParameters.getReplaceWhitespaceWith());
+        assertEquals(DefaultIgnoreCase, autoPhrasingParameters.getIgnoreCase());
+        assertEquals(DefaultPhraseSetFiles, autoPhrasingParameters.getPhraseSetFiles());
+        assertEquals(0, autoPhrasingParameters.getIndividualPhraseSetFiles().size());
+        assertEquals(DefaultEmitSingleTokens, autoPhrasingParameters.getEmitSingleTokens());
+    }
+
+    public void testConstructorWithSolrParams() {
+        SolrParams solrParams = getSolrParamsOnePhraseFile();
+        AutoPhrasingParameters autoPhrasingParameters = new AutoPhrasingParameters(solrParams);
+        assertNotNull(autoPhrasingParameters);
+        assertEquals(DownstreamParser, autoPhrasingParameters.getDownstreamParser());
+        assertEquals(ReplaceWhitespaceWith, autoPhrasingParameters.getReplaceWhitespaceWith());
+        assertEquals(IgnoreCase, autoPhrasingParameters.getIgnoreCase());
+        assertEquals(PhrasesOneFile, autoPhrasingParameters.getPhraseSetFiles());
+        assertEquals(1, autoPhrasingParameters.getIndividualPhraseSetFiles().size());
+        assertEquals(PhrasesOneFile, autoPhrasingParameters.getIndividualPhraseSetFiles().get(0));
+        assertEquals(EmitSingleTokens, autoPhrasingParameters.getEmitSingleTokens());
+    }
+
+    public void testConstructorNoIgnoreCase(){
+        SolrParams solrParams = getSolrParamsNoIgnoreCase();
+        AutoPhrasingParameters autoPhrasingParameters = new AutoPhrasingParameters(solrParams);
+        assertNotNull(autoPhrasingParameters);
+        assertEquals(DownstreamParser, autoPhrasingParameters.getDownstreamParser());
+        assertEquals(ReplaceWhitespaceWith, autoPhrasingParameters.getReplaceWhitespaceWith());
+        assertEquals(DefaultIgnoreCase, autoPhrasingParameters.getIgnoreCase());
+        assertEquals(PhrasesOneFile, autoPhrasingParameters.getPhraseSetFiles());
+        assertEquals(1, autoPhrasingParameters.getIndividualPhraseSetFiles().size());
+        assertEquals(PhrasesOneFile, autoPhrasingParameters.getIndividualPhraseSetFiles().get(0));
+        assertEquals(EmitSingleTokens, autoPhrasingParameters.getEmitSingleTokens());
+    }
+
+    public void testConstructorNoEmitSingleTokens(){
+        SolrParams solrParams = getSolrParamsNoEmitSingleTokens();
+        AutoPhrasingParameters autoPhrasingParameters = new AutoPhrasingParameters(solrParams);
+        assertNotNull(autoPhrasingParameters);
+        assertEquals(DownstreamParser, autoPhrasingParameters.getDownstreamParser());
+        assertEquals(ReplaceWhitespaceWith, autoPhrasingParameters.getReplaceWhitespaceWith());
+        assertEquals(IgnoreCase, autoPhrasingParameters.getIgnoreCase());
+        assertEquals(1, autoPhrasingParameters.getIndividualPhraseSetFiles().size());
+        assertEquals(PhrasesOneFile, autoPhrasingParameters.getPhraseSetFiles());
+        assertEquals(PhrasesOneFile, autoPhrasingParameters.getIndividualPhraseSetFiles().get(0));
+        assertEquals(DefaultEmitSingleTokens, autoPhrasingParameters.getEmitSingleTokens());
+    }
+
+    public void testConstructorWithSolrParamsMultiplePhraseSetFiles() {
+        SolrParams solrParams = getSolrParamsMultiplePhraseFiles();
+        AutoPhrasingParameters autoPhrasingParameters = new AutoPhrasingParameters(solrParams);
+        assertNotNull(autoPhrasingParameters);
+        assertEquals(DownstreamParser, autoPhrasingParameters.getDownstreamParser());
+        assertEquals(ReplaceWhitespaceWith, autoPhrasingParameters.getReplaceWhitespaceWith());
+        assertEquals(IgnoreCase, autoPhrasingParameters.getIgnoreCase());
+        assertEquals(PhrasesMultipleFiles, autoPhrasingParameters.getPhraseSetFiles());
+        assertEquals(2, autoPhrasingParameters.getIndividualPhraseSetFiles().size());
+        assertEquals("phrases.txt", autoPhrasingParameters.getIndividualPhraseSetFiles().get(0));
+        assertEquals("more_phrases.txt", autoPhrasingParameters.getIndividualPhraseSetFiles().get(1));
+    }
+
+    private SolrParams getSolrParamsOnePhraseFile() {
+        NamedList params  = new NamedList();
+        params.add("defType", DownstreamParser);
+        params.add("replaceWhitespaceWith", ReplaceWhitespaceWith);
+        params.add("ignoreCase", IgnoreCase);
+        params.add("phrases", PhrasesOneFile);
+        params.add("includeTokens", EmitSingleTokens);
+
+        return SolrParams.toSolrParams(params);
+    }
+
+    private SolrParams getSolrParamsMultiplePhraseFiles() {
+        NamedList params  = new NamedList();
+        params.add("defType", DownstreamParser);
+        params.add("replaceWhitespaceWith", ReplaceWhitespaceWith);
+        params.add("ignoreCase", IgnoreCase);
+        params.add("phrases", PhrasesMultipleFiles);
+        params.add("includeTokens", EmitSingleTokens);
+
+        return SolrParams.toSolrParams(params);
+    }
+
+    private SolrParams getSolrParamsNoIgnoreCase() {
+        NamedList params  = new NamedList();
+        params.add("defType", DownstreamParser);
+        params.add("replaceWhitespaceWith", ReplaceWhitespaceWith);
+        params.add("phrases", PhrasesOneFile);
+        params.add("includeTokens", EmitSingleTokens);
+
+        return SolrParams.toSolrParams(params);
+    }
+
+    private SolrParams getSolrParamsNoEmitSingleTokens() {
+        NamedList params  = new NamedList();
+        params.add("defType", DownstreamParser);
+        params.add("replaceWhitespaceWith", ReplaceWhitespaceWith);
+        params.add("ignoreCase", IgnoreCase);
+        params.add("phrases", PhrasesOneFile);
+
+        return SolrParams.toSolrParams(params);
+    }
+}
